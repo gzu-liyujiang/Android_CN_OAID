12eba4c3a632e29bd925be675f9bbe1d2fa88cd3
gzu-liyujiang <1032694760@qq.com>
2023-11-02 01:01:18 +0800

    升级 JDK、Android Studio、Gradle、Android Gradle plugin、Android SDK Build Tools 到最新稳定版本

--- buildAPK.bat
+++ buildAPK.bat
@@ -1,4 +1,5 @@
-@rem JAVA_HOME -> jdk1.8
+@rem JAVA_HOME -> Gradle 7 require JDK 11+, Gradle 8 require JDK 17+
+@rem See https://docs.gradle.org/current/userguide/compatibility.html
 @echo build and proguard apk
 cd /d ./
 gradlew.bat clean assembleRelease --info --warning-mode all
--- publishAAR.bat
+++ publishAAR.bat
@@ -1,4 +1,5 @@
-@rem JAVA_HOME -> jdk1.8
+@rem JAVA_HOME -> Gradle 7 require JDK 11+, Gradle 8 require JDK 17+
+@rem See https://docs.gradle.org/current/userguide/compatibility.html
 @echo publish aar to local repository
 cd /d ./
 gradlew.bat clean publishReleasePublicationToLocalRepository --info --warning-mode all
