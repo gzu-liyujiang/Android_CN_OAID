13eb1132457467e92cf5551ea488d239420eb094
gzu-liyujiang <1032694760@qq.com>
2026-03-21 23:46:42 +0800

    最新安装包

--- buildAPK.bat
+++ buildAPK.bat
@@ -1,7 +1,6 @@
-@rem JAVA_HOME -> Gradle 7 require JDK 11+, Gradle 8 require JDK 17+
-@rem See https://docs.gradle.org/current/userguide/compatibility.html
+chcp 65001
+
+@rem JAVA_HOME -> jdk17
 @echo build and proguard apk
 cd /d ./
-gradlew.bat clean assembleRelease --info --warning-mode all
-pause
-
+start gradlew.bat clean assembleRelease --info --warning-mode all
--- publishAAR.bat
+++ publishAAR.bat
@@ -1,7 +1,7 @@
-@rem JAVA_HOME -> Gradle 7 require JDK 11+, Gradle 8 require JDK 17+
-@rem See https://docs.gradle.org/current/userguide/compatibility.html
+chcp 65001
+
+@rem JAVA_HOME -> jdk17
 @echo publish aar to local repository
 cd /d ./
-gradlew.bat clean publishReleasePublicationToLocalRepository --info --warning-mode all
-pause
+start gradlew.bat clean publishReleasePublicationToLocalRepository --info --warning-mode all
 
